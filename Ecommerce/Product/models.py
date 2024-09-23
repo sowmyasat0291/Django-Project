0d8012df17525e53182677e4f0412786c926dd99
Sowmya <sowmyam0291@gmail.com>
2024-09-23 14:19:01 +0530

    Test Commit

--- Ecommerce/Product/models.py
+++ Ecommerce/Product/models.py
@@ -6,8 +6,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)  # Set default to 0
     description = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
-
+    
     def __str__(self):
         return self.name
