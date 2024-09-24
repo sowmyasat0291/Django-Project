e2f8600d1b77c63b00d399a1bf6993c6cfe57f76
Sowmya <sowmyam0291@gmail.com>
2024-09-24 12:17:57 +0530

    Test Commit

--- Ecommerce/Cart/urls.py
+++ Ecommerce/Cart/urls.py
@@ -1,7 +1,9 @@
+# cart/urls.py
 from django.urls import path
-from .views import add_to_cart, cart_detail
+from . import views
 
 urlpatterns = [
-    path('add/<int:product_id>/', add_to_cart, name='add_to_cart'),
-    path('cart/', cart_detail, name='cart_detail'),
+    path('', views.cart_detail, name='cart_detail'),
+    path('add/<int:product_id>/', views.cart_add, name='cart_add'),
+    path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
 ]
--- Ecommerce/Cart/views.py
+++ Ecommerce/Cart/views.py
@@ -1,9 +1,11 @@
 # cart/views.py
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Cart, CartItem
 from Product.models import Product
 
-def add_to_cart(request, product_id):
+@login_required
+def cart_add(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     cart, created = Cart.objects.get_or_create(user=request.user)
     cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
@@ -14,8 +16,16 @@ def add_to_cart(request, product_id):
     
     return redirect('cart_detail')
 
+@login_required
 def cart_detail(request):
     cart, created = Cart.objects.get_or_create(user=request.user)
     items = CartItem.objects.filter(cart=cart)
     
     return render(request, 'cart/cart_detail.html', {'items': items})
+@login_required
+def cart_remove(request, product_id):
+    cart = get_object_or_404(Cart, user=request.user)
+    cart_item = get_object_or_404(CartItem, cart=cart, product_id=product_id)
+    cart_item.delete()
+    
+    return redirect('cart_detail')
